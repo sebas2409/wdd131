dcab75e41dba32bbc9297de7ae03d1391b3ab6d2
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-05-14 15:07:46 -0600

    fix

--- ponder/responsive-menu/responsive.js
+++ ponder/responsive-menu/responsive.js
@@ -3,4 +3,13 @@ menuButton.addEventListener("click", (e) => {
     let menu = document.querySelector('.menu');
     menu.classList.toggle('hide');
     menuButton.classList.toggle('change');
+
+    menu.style.display = 'flex';
+    menu.style.flexDirection = 'column';
+    menu.style.alignItems = 'center';
+    menu.style.justifyContent = 'center';
+
+    //each a fit hole width div
+    menu.style.width = '100%';
+
 })
