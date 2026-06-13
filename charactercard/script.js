71e43ed11184786c044d2f6a53c681a99e002526
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-06-13 10:42:55 -0600

    character card

---
+++ charactercard/script.js
@@ -0,0 +1,43 @@
+const character = {
+    name: "Snortleblat",
+    charClass: "Swamp Beast Diplomat",
+    level: 5,
+    health: 100,
+
+    updateUI: function () {
+        document.getElementById('charName').innerText = this.name;
+        document.getElementById('charClass').innerText = this.charClass;
+        document.getElementById('charLevel').innerText = this.level;
+        document.getElementById('charHealth').innerText = this.health;
+        document.getElementById('charImage').src = this.image;
+    },
+
+
+    attacked: function () {
+        if (this.health > 0) {
+            this.health -= 20;
+
+            if (this.health <= 0) {
+                this.health = 0;
+                this.updateUI();
+                setTimeout(() => alert(`${this.name} has died!`), 10);
+            } else {
+                this.updateUI();
+            }
+        }
+    },
+
+    // Método para subir de nivel
+    levelUp: function () {
+        if (this.health > 0) {
+            this.level += 1;
+            this.updateUI();
+        } else {
+            alert(`${this.name} cannot level up because they are dead.`);
+        }
+    }
+};
+
+window.onload = function () {
+    character.updateUI();
+};
